fbbc5afa6eda5c250d17d459ec3b470fbfcfa0ba
Martin Nilsson <martinnilss@gmail.com>
2016-01-20 13:17:33 +0100

    la till en test.js

--- test.js
+++ test.js
@@ -5,9 +5,12 @@ function getClass(c){
 }
 
 function alertAllNames(pDiv){
+
 	var len = pDiv.length;
 	var names = [];
+
 	for (var i = len - 1; i >= 0; i--) {
+		
 		names.push(pDiv[i].childNodes[1].innerHTML+" ");
 	} 
 
